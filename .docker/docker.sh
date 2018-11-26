695cc9411f32601b633feb1078557d085c297ab5
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-11-26 21:41:57 +0100

    - OTP_VERSION="21.1.3" - clean up arm32v7

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -119,12 +119,10 @@ docker_manifest_list_version() {
   docker manifest create ${TARGET}:${BUILD_VERSION} \
       ${TARGET}:${BUILD_VERSION}-alpine-amd64 \
       ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 \
-      # ${TARGET}:${BUILD_VERSION}-slim-arm32v7 \
       ${TARGET}:${BUILD_VERSION}-alpine-arm64v8
 
   # Manifest Annotate BUILD_VERSION
   docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-  # docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-slim-arm32v7 --os=linux --arch=arm --variant=v7
   docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
   # Manifest Push BUILD_VERSION
@@ -137,12 +135,10 @@ docker_manifest_list_latest() {
   docker manifest create ${TARGET}:latest \
     ${TARGET}:${BUILD_VERSION}-alpine-amd64 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 \
-    # ${TARGET}:${BUILD_VERSION}-slim-arm32v7 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm64v8
 
   # Manifest Annotate BUILD_VERSION
   docker manifest annotate ${TARGET}:latest ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-  # docker manifest annotate ${TARGET}:latest ${TARGET}:${BUILD_VERSION}-slim-arm32v7 --os=linux --arch=arm --variant=v7
   docker manifest annotate ${TARGET}:latest ${TARGET}:${BUILD_VERSION}-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
   # Manifest Push BUILD_VERSION
@@ -155,12 +151,10 @@ docker_manifest_list_beta() {
   docker manifest create ${TARGET}:beta \
     ${TARGET}:${BUILD_VERSION}-alpine-amd64 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 \
-    # ${TARGET}:${BUILD_VERSION}-slim-arm32v7 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm64v8
 
   # Manifest Annotate BUILD_VERSION
   docker manifest annotate ${TARGET}:beta ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-  # docker manifest annotate ${TARGET}:beta ${TARGET}:${BUILD_VERSION}-slim-arm32v7 --os=linux --arch=arm --variant=v7
   docker manifest annotate ${TARGET}:beta ${TARGET}:${BUILD_VERSION}-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
   # Manifest Push BUILD_VERSION
@@ -173,12 +167,10 @@ docker_manifest_list_testing() {
   docker manifest create ${TARGET}:testing \
     ${TARGET}:${BUILD_VERSION}-alpine-amd64 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 \
-    # ${TARGET}:${BUILD_VERSION}-slim-arm32v7 \
     ${TARGET}:${BUILD_VERSION}-alpine-arm64v8
 
   # Manifest Annotate BUILD_VERSION
   docker manifest annotate ${TARGET}:testing ${TARGET}:${BUILD_VERSION}-alpine-arm32v6 --os=linux --arch=arm --variant=v6
-  # docker manifest annotate ${TARGET}:testing ${TARGET}:${BUILD_VERSION}-slim-arm32v7 --os=linux --arch=arm --variant=v7
   docker manifest annotate ${TARGET}:testing ${TARGET}:${BUILD_VERSION}-alpine-arm64v8 --os=linux --arch=arm64 --variant=v8
 
   # Manifest Push BUILD_VERSION
@@ -205,17 +197,6 @@ docker_manifest_list_version_os_arch() {
   # Manifest Push alpine-arm32v6
   docker manifest push ${TARGET}:${BUILD_VERSION}-alpine-arm32v6
 
-  # # Manifest Create slim-arm32v7
-  # echo "DOCKER MANIFEST: Create and Push docker manifest list - ${TARGET}:${BUILD_VERSION}-slim-arm32v7."
-  # docker manifest create ${TARGET}:${BUILD_VERSION}-slim-arm32v7 \
-  #   ${TARGET}:${BUILD_VERSION}-slim-arm32v7
-  #
-  # # Manifest Annotate slim-arm32v7
-  # docker manifest annotate ${TARGET}:${BUILD_VERSION}-slim-arm32v7 ${TARGET}:${BUILD_VERSION}-slim-arm32v7 --os=linux --arch=arm --variant=v7
-  #
-  # # Manifest Push slim-arm32v7
-  # docker manifest push ${TARGET}:${BUILD_VERSION}-slim-arm32v7
-
   # Manifest Create alpine-arm64v8
   echo "DOCKER MANIFEST: Create and Push docker manifest list - ${TARGET}:${BUILD_VERSION}-alpine-arm64v8."
   docker manifest create ${TARGET}:${BUILD_VERSION}-alpine-arm64v8 \
@@ -232,12 +213,7 @@ setup_dependencies() {
   echo "PREPARE: Setting up dependencies."
 
   sudo apt update -y
-  # sudo apt install realpath python python-pip -y
   sudo apt install --only-upgrade docker-ce -y
-  # sudo pip install docker-compose || true
-  #
-  # docker info
-  # docker-compose --version
 }
 
 update_docker_configuration() {
